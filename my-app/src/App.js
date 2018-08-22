5ad3fb28a5a42706af095d961459abc5ae129629
DESKTOP-LISIJ92\tejam <ravistack4@gmail.com>
2018-08-21 22:20:18 -0500

    Intro Page Updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,11 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">RCL Exceptions_UI</h1>
         </header>
-        <p className="App-intro">
-          RCL Exceptions-UI
-        </p>
       </div>
     );
   }
